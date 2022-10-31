e46ce36a791be4069e081b37ced8eeb3ade9c4c6
og og <og373652@gmail.com>
2022-10-31 08:11:44 +0200

    w

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -2,28 +2,65 @@
 import { allIds, fetchOrderById } from "../api";
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
+const DAYS_RANGE = 14;
+const MS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
 
-const fetchAllOrders = () => {
+const safeAddToMap = (map, key, value) => {
+    if (!map[key]) {
+        map[key] = [];
+    } 
+    map[key].push(value);
+}
+const getAmountOfDaysFromNowToDate = (date) => {
+    const today = new Date();
+    const dateToCheck = new Date(date);
+    const timeDiffInMs = today - dateToCheck;
+    const daysDiff = timeDiffInMs / MS_IN_SECOND / SECONDS_IN_MINUTE / MINUTES_IN_HOUR / HOURS_IN_DAY;
+    const roundedDaysDiff = Math.ceil(daysDiff);
+    return roundedDaysDiff;
+}
+
+export const fetchAllOrders = async() => {
     const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+    const allOrdersData = await Promise.all(ids.map((id) => fetchOrderById(id)));
+    return allOrdersData;
 };
 
-const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
+export const bucketOrdersByUsers = async() => {
     //   2. TODO: using the function from section 1 you should now bucket the orders by user.
     // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+    let ordersByUsers = {};
+    const allOrders = await fetchAllOrders();
+    allOrders.forEach((order) => {
+        const { userId }  = order;
+        safeAddToMap(ordersByUsers, userId, order);
+    });
     return ordersByUsers;
 };
 
-const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+export const getLast2WeeksOrders = async() => {
+    const allOrders = await fetchAllOrders();
+    const ordersFromLastTwoWeeks = allOrders.filter((order) => {
+        const { timestamp } = order;
+        const daysDiff = getAmountOfDaysFromNowToDate(timestamp);
+        return daysDiff <= DAYS_RANGE;
+    });
+    return ordersFromLastTwoWeeks;
 };
 
-const bucketOrdersByDate = () => {
+export const bucketOrdersByDate = async() => {
     let ordersByDate = {};
     //   4. TODO: using the function from section 3 bucket the orders by date.
     // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+    const lastTwoWeeksOrders = await getLast2WeeksOrders();
+    lastTwoWeeksOrders.forEach((order) => {
+        const { timestamp } = order;
+        const daysFromNow = getAmountOfDaysFromNowToDate(timestamp);
+        safeAddToMap(ordersByDate, daysFromNow, order);
+    });
     return ordersByDate;
 };
 
@@ -37,6 +74,7 @@ getLast2WeeksOrders();
 // .then(console.log);
 
 bucketOrdersByDate();
+
 // .then(console.log);
 
 ////////////////////////////////////////
--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -1,7 +1,25 @@
 import { fetchOrderById } from "../api";
+import { bucketOrdersByDate, bucketOrdersByUsers, fetchAllOrders, getLast2WeeksOrders } from "./ecommerce";
 
 const ORDER_ID = "70ef599e5eca171b2bce84d1"
+const EXPECTED_AMOUNT_OF_ORDERS = 100;
 test("Ecommerce - fetchOrderById", async () => {
     let orders = await fetchOrderById(ORDER_ID);
     expect(orders).toBeTruthy();
 });
+test('Can fetch all orders', async () => {
+    const allOrders = await fetchAllOrders();
+    expect(allOrders.length).toBe(EXPECTED_AMOUNT_OF_ORDERS);
+})
+test('Can get user order bucket, mapped by userId', async () => {
+    const userToOrderMap = await bucketOrdersByUsers();
+    expect(userToOrderMap).toBeTruthy();
+});
+test('Can get orders from last 2 weeks', async () => {
+    const relevantOrders = await getLast2WeeksOrders();
+    expect(relevantOrders).toBeTruthy();
+})
+test('Can get orders bucket by day', async() => {
+    const orderByDayBucket = await bucketOrdersByDate();
+    console.log(orderByDayBucket);
+})
