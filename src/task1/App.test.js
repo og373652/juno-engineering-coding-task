a0acaab7edbffb0c8c067fb5b8182dfc15c0aaff
og og <117059504+og373652@users.noreply.github.com>
2022-11-01 09:02:54 +0200

    Update App.test.js

--- src/task1/App.test.js
+++ src/task1/App.test.js
@@ -4,6 +4,5 @@ import App from "./App";
 test("Testing works!", () => {
   const { getByText } = render(<App />);
   // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(getByText(/Your code/i)).toBeInTheDocument();
 });
 
